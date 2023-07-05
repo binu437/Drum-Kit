81287fa55c465cf64d042dca2702ba5c73a120a9
binu437 <114605503+binu437@users.noreply.github.com>
2023-07-05 17:23:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,106 @@
+
+// for loop method to add event to all button
+// DETECTING BUTTON PRESS
+
+var numberOfDrumButtons=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfDrumButtons;i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
+    
+    function handleClick(){
+
+    //    THIS DENOTE THE BUTTON WHICH IS CLICKED
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+    }
+}
+// DETECTING KEYBOARD PRESS
+
+     // adding keyboard press to entire document
+    //  event denote which button got trigged(similiar to this)
+
+document.addEventListener("keypress",function(event){
+   
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+
+// FUNCTION FOR BOTH
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+
+        break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+
+        break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+
+        break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+
+        break;
+
+        case "j":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+
+        break;
+
+        case "k":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+
+        break;
+
+        case "l":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+
+        break;
+  
+        default:
+            console.log(buttonInnerHTML);
+            
+      }
+
+}
+
+
+function buttonAnimation(currentkey){
+    
+    var activeButton = document.querySelector("."+currentkey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+
+
+
+}
+
+// // anomonys function (alternate func of above one)
+// document.querySelector("button").addEventListener("click",function (){
+// alert("i got clicked");
+// });  
